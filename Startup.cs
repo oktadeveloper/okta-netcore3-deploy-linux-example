58352da3f305f9183e8adfe64dacc6aed270e202
sige <sige@configcat.com>
2020-10-01 09:40:51 +0200

    https

--- Startup.cs
+++ Startup.cs
@@ -81,7 +81,7 @@ namespace okta_netcore3_deploy_to_cloud_hosts_example
             //     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
             //     app.UseHsts();
             // }
-            // app.UseHttpsRedirection();
+            app.UseHttpsRedirection();
             app.UseStaticFiles();
 
             app.UseRouting();
